4e6e14b4c78d6bc24306afca08da0997334d9ba2
Austin Pennyfeather <apennyfeather@ics.com>
2021-09-21 17:35:27 -0400

    Implemented service worker

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = ({ covid }) => {
     return (
         <div className='header'>
             <div>
-                <span className='d-inline-block'><img className='flag'src="https://disease.sh/assets/img/flags/kn.png" alt="Saint Kitts And Nevis National Flag"/></span>
+                <span className='d-inline-block'><img className='flag'src="./logo192.png" alt="Saint Kitts And Nevis National Flag"/></span>
                 <span className='d-inline-block btn float-right headerStyle'>{covid.All?.country} COVID-19 Report</span>
             </div>
             <div className='population'>Population: {covid.All?.population?.toLocaleString("en-US")}</div>
